6ff5c006d60e4d222e8b418cecdf720313d308ea
Pål aronsson <aronsson02.p@gmail.com>
2020-08-24 09:53:03 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,16 +1,17 @@
-﻿sudo cp "/home/yami/Documents/фэлш/evdev.xml" "/usr/share/X11/xkb/rules/"
-sudo cp "/home/yami/Documents/фэлш/jp" "/usr/share/X11/xkb/symbols"
+﻿sudo cp "~/dot-files/XKB/evdev.xml" "/usr/share/X11/xkb/rules/"
+sudo cp "~/dot-files/XKB/jp" "/usr/share/X11/xkb/symbols"
 setxkbmap jp
 sudo mkdir /Archive
 sudo mkdir /Anime
 sudo mkdir /Storage
-sudo ln -sf "/home/yami/Documents/фэлш/Themes/faev kde splash" "/home/yami/.local/share/plasma/look-and-feel/faev"
-sudo ln -sf "/home/yami/Documents/фэлш/Themes/faev plasma" "/home/yami/.local/share/plasma/desktoptheme/faev"
-sudo ln -sf "/home/yami/Documents/фэлш/Themes/faev plasma/ðэв литэв.colors" "/home/yami/.local/share/color-schemes"
-sudo ln -sf "/home/yami/Documents/фэлш/Themes/faev kde" "/home/yami/.local/share/aurorae/themes/faev"
-sudo ln -sf "/home/yami/Documents/фэлш/Themes/faev" "/usr/share/themes/faev"
-sudo ln -sf "/home/yami/Documents/фэлш/Themes/faev IC" "/usr/share/icons/faev"
-sudo ln -sf "/home/yami/Documents/фэлш/FFchrome" "/home/yami/.mozilla/firefox/zpba42s9.default/chrome/"
+sudo ln -sf "~/Documents/фэлш/Themes/faev kde splash" "~/.local/share/plasma/look-and-feel/faev"
+sudo ln -sf "~/Documents/фэлш/Themes/faev plasma" "~/.local/share/plasma/desktoptheme/faev"
+sudo ln -sf "~/Documents/фэлш/Themes/faev plasma/ðэв литэв.colors" "~/.local/share/color-schemes"
+sudo ln -sf "~/Documents/фэлш/Themes/faev kde" "~/.local/share/aurorae/themes/faev"
+sudo ln -sf "~/dot-files/rice/linux system/themes/Story" "~/.themes/Story"
+sudo ln -sf "~/dot-files/rice/linux system/icons/Story" "~/.icons/Story"
+sudo ln -sf "~/dot-files/rice/linux system/icons/blue-oreo" "~/.icons/blue-oreo"
+sudo ln -sf "~/dot-files/rice/firefox" "~/.mozilla/firefox/zpba42s9.default/chrome/"
 sudo pacman -Sy jack qjackctl pulseaudio-jack pulseeffects pulseaudio-qt yoshimi fontforge ktorrent gparted virt-manager qemu rawtherapee wine wine-mono wine-gecko winetricks opencl-mesa otf-ipafont
 yay -Sy fontpreview ckb-next minecraft-launcher-beta qemu-spice stress-ng youtube-dl
 sudo systemctl enable ckb-next-daemon
