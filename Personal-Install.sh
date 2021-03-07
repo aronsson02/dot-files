08da8ebe35413359ccc3150a13b99cdd5279d742
aronsson02 <aronsson02.p@gmail.com>
2021-03-07 19:06:04 +0100

    avrs24

--- Personal-Install.sh
+++ Personal-Install.sh
@@ -1,3 +1,3 @@
 sudo mkdir /Archive
 sudo mkdir /Anime
-yay -Sy ckb-next-git yoshimi fontforge ktorrent gparted rawtherapee wine wine-mono wine-gecko chromium inkscape-git whatsapp-for-linux baobab isousb
+yay -Sy dolphin-git kate-git ckb-next-git fontforge ktorrent gparted rawtherapee wine wine-mono wine-gecko chromium inkscape-git whatsapp-for-linux baobab isousb
