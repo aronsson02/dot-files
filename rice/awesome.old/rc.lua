4acafa9bb7fc0ec18e0c5bf9eb19075be87517cb
aronsson02 <aronsson02.p@gmail.com>
2022-06-08 14:55:12 +0200

    7ig8e4o

---
+++ rice/awesome.old/rc.lua
@@ -0,0 +1,70 @@
+-- ░█▀▀░█░█░█▀▄░█▀▄░█▀▀░█▀█░█░░
+-- ░▀▀█░█░█░█▀▄░█▀▄░█▀▀░█▀█░█░░
+-- ░▀▀▀░▀▀▀░▀░▀░▀░▀░▀▀▀░▀░▀░▀▀▀
+
+local gears = require('gears')
+local beautiful = require('beautiful')
+require('awful.autofocus')
+
+-- ░▀█▀░█░█░█▀▀░█▄█░█▀▀
+-- ░░█░░█▀█░█▀▀░█░█░█▀▀
+-- ░░▀░░▀░▀░▀▀▀░▀░▀░▀▀▀
+
+beautiful.init(require("theme"))
+
+-- ░█░░░█▀█░█░█░█▀█░█░█░▀█▀
+-- ░█░░░█▀█░░█░░█░█░█░█░░█░
+-- ░▀▀▀░▀░▀░░▀░░▀▀▀░▀▀▀░░▀░
+
+require('layout')
+
+-- ░█▀▀░█▀█░█▀█░█▀▀░▀█▀░█▀▀░█░█░█▀▄░█▀█░▀█▀░▀█▀░█▀█░█▀█░█▀▀
+-- ░█░░░█░█░█░█░█▀▀░░█░░█░█░█░█░█▀▄░█▀█░░█░░░█░░█░█░█░█░▀▀█
+-- ░▀▀▀░▀▀▀░▀░▀░▀░░░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀░▀░░▀░░▀▀▀░▀▀▀░▀░▀░▀▀▀
+
+require('configuration.client')
+require('configuration.root')
+require('configuration.tags')
+root.keys(require('configuration.keys.global'))
+
+-- ░█▄█░█▀█░█▀▄░█░█░█░░░█▀▀░█▀▀
+-- ░█░█░█░█░█░█░█░█░█░░░█▀▀░▀▀█
+-- ░▀░▀░▀▀▀░▀▀░░▀▀▀░▀▀▀░▀▀▀░▀▀▀
+
+require('module.notifications')
+require('module.auto-start')
+require('module.backdrop')
+require('module.decorate-client')
+require('module.exit-screen')
+require('module.menu')
+require('module.titlebar')
+require('module.brightness-osd')
+require('module.volume-osd')
+require('module.dynamic-wallpaper')
+
+-- ░█░█░█▀█░█░░░█░░░█▀█░█▀█░█▀█░█▀▀░█▀▄
+-- ░█▄█░█▀█░█░░░█░░░█▀▀░█▀█░█▀▀░█▀▀░█▀▄
+-- ░▀░▀░▀░▀░▀▀▀░▀▀▀░▀░░░▀░▀░▀░░░▀▀▀░▀░▀
+
+screen.connect_signal(
+    'request::wallpaper',
+    function(s)
+        -- If wallpaper is a function, call it with the screen
+        if beautiful.wallpaper then
+            if type(beautiful.wallpaper) == 'string' then
+
+                -- Check if beautiful.wallpaper is color/image
+                if beautiful.wallpaper:sub(1, #'#') == '#' then
+                    -- If beautiful.wallpaper is color
+                    gears.wallpaper.set(beautiful.wallpaper)
+
+                elseif beautiful.wallpaper:sub(1, #'/') == '/' then
+                    -- If beautiful.wallpaper is path/image
+                    gears.wallpaper.maximized(beautiful.wallpaper, s)
+                end
+            else
+                beautiful.wallpaper(s)
+            end
+        end
+    end
+)
---
+++ rice/awesome/components/mirage/titlebar.lua
@@ -0,0 +1,41 @@
+--      ████████╗██╗████████╗██╗     ███████╗██████╗  █████╗ ██████╗
+--      ╚══██╔══╝██║╚══██╔══╝██║     ██╔════╝██╔══██╗██╔══██╗██╔══██╗
+--         ██║   ██║   ██║   ██║     █████╗  ██████╔╝███████║██████╔╝
+--         ██║   ██║   ██║   ██║     ██╔══╝  ██╔══██╗██╔══██║██╔══██╗
+--         ██║   ██║   ██║   ███████╗███████╗██████╔╝██║  ██║██║  ██║
+--         ╚═╝   ╚═╝   ╚═╝   ╚══════╝╚══════╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝
+
+-- ===================================================================
+-- Initialization
+-- ===================================================================
+
+
+local awful = require("awful")
+local wibox = require("wibox")
+local dpi = require("beautiful").xresources.apply_dpi
+
+
+-- ===================================================================
+-- Titlebar Creation
+-- ===================================================================
+
+
+-- Add a titlebar
+client.connect_signal("request::titlebars", function(c)
+   local titlebar = awful.titlebar(c, {
+      size = dpi(26)
+   })
+
+   titlebar: setup {
+      {
+         -- AwesomeWM native buttons (images loaded from theme)
+         wibox.container.margin(awful.titlebar.widget.closebutton(c), dpi(10), dpi(5), dpi(5), dpi(5)),
+         wibox.container.margin(awful.titlebar.widget.maximizedbutton(c), dpi(5), dpi(5), dpi(5), dpi(5)),
+         wibox.container.margin(awful.titlebar.widget.minimizebutton(c), dpi(5), dpi(5), dpi(5), dpi(5)),
+         layout = wibox.layout.fixed.horizontal
+      },
+      nil,
+      nil,
+      layout = wibox.layout.align.horizontal
+   }
+end)
