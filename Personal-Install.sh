cafab9c1a6b4fdcc12a9924c79cc286432086d0b
aronsson02 <aronsson02.p@gmail.com>
2022-10-22 23:35:32 +0200

    Test

--- Personal-Install.sh
+++
@@ -1,3 +0,0 @@
-sudo mkdir /Archive
-sudo mkdir /Anime
-yay -Sy dolphin-git kate-git ckb-next-git fontforge ktorrent gparted rawtherapee wine wine-mono wine-gecko chromium inkscape-git whatsapp-for-linux baobab isousb
