efac215aafab78e3f793a3faef7fbebf4b7ceddb
aronsson02 <aronsson02.p@gmail.com>
2024-03-16 12:42:54 +0100

    z71o6qn

--- Install.sh
+++ Install.sh
@@ -1,13 +1,20 @@
 
+git clone https://github.com/aronsson02/dot-files.git
+
+yay -Sy doas
+
 cp "$HOME/dot-files/doas/doas.conf" "/etc/doas.conf"
-
-yay -Y --builddir /Archive/arch-pkg/yay --save
-yay -Sy doas hyprland-git waybar-hyprland-git dunst-git swaybg-git swaylock-effects-git swayidle-git rofi-git qt5ct xdg-user-dirs kvantum-qt5-git ckb-next-git fontforge rawtherapee inkscape baobab isousb czkawka-gui czkawka-cli easyeffects piper-git yt-dlp partitionmanager
-
-# yay -Sy awesome-git picom-tryone-git
 
 doas mkdir /Archive
 doas mkdir /Tera
+
+yay -Y --builddir /Archive/arch-pkg/yay --save
+
+yay -Sy zsh hyprland-git waybar-hyprland-git dunst-git swaybg-git swaylock-effects-git swayidle-git rofi-git qt5ct qt6ct xdg-user-dirs kvantum-git ckb-next-git fontforge rawtherapee inkscape baobab isousb czkawka-gui czkawka-cli easyeffects piper-git yt-dlp partitionmanager
+
+chsh -s /bin/zsh
+
+# yay -Sy awesome-git picom-tryone-git
 
 # doas cp "$HOME/dot-files/X/00-mouse.conf" "/etc/X11/xorg.conf.d"
 doas cp "$HOME/dot-files/X/no" "/usr/share/X11/xkb/symbols"
@@ -22,3 +29,4 @@ hyprctl reload
 # ln -sf "$HOME/dot-files/rice/GTK/Reverence" "$HOME/.themes/Reverence"
 # ln -sf "$HOME/dot-files/rice/icons/Reverence" "$HOME/.icons/Reverence"
 # ln -sf "$HOME/dot-files/rice/Kvantum/" "$HOME/.config/Kvantum"
+# ln -sf "$HOME/dot-files/ZSH/.zshrc" "$HOME/.zshrc"
